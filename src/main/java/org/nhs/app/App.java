c1cc5c5730a574832e631db759d3b6ba731df76e
11111594 <2006877@apps.nsd.org>
2024-10-07 21:05:57 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,66 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-package org.nhs.app;
-
-// Your code goes here
+public class App{
+    public static void main(String args[]){
+        nest1();
+        System.out.println();
+        nest2();
+        System.out.println();
+        nest3();
+        System.out.println();
+        challenge();
+    }
+    public static void nest1(){
+        for(int i=1; i<=5; i++){
+            for(int j=0; j<=i; j++){
+                for(int k=0; j<i; j++){
+                    System.out.print(i);
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void nest2(){
+        for(int i=1; i<=5; i++){
+            for(int k=5; k>i; k--){
+                System.out.print(".");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void nest3(){
+        for(int i=1; i<=5; i++){
+            for(int k=5; k>i; k--){
+                System.out.print(".");
+            }
+            System.out.print(i);
+            for(int j=1; j<i; j++){
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+    public static void challenge(){
+        for(int i=1; i<=9; i++){
+            for(int k=9; k>i; k--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(i);
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        for(int i=0; i<=8; i++){
+            for(int k=0; k<=i; k++){
+                System.out.print(" ");
+            }
+            for(int j=7; j>=i; j--){
+                System.out.print(8-i);
+                System.out.print(8-i);
+            }
+            System.out.println();
+        }
+    }
+}
